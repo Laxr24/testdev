f761efdd119b3927b333c7132e34cc39ce04377d
Laxr24 <shahanuzzamans@gmail.com>
2023-02-07 23:53:12 +0600

    __test

--- Test.java
+++ Test.java
@@ -1,10 +1,13 @@
 public class Test{
     public static void main(String[] args) { 
-        int sum = Add(200, 222); 
-        System.out.println("The Sum is: "+ sum);
+        // int sum = Add(200, 222); 
+        // System.out.println("The Sum is: "+ sum);
+
+        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("*566#"))); 
     }
-    public static int Add(int firstNumber, int SecondNumber){
-        int sum = firstNumber+SecondNumber; 
-        return sum; 
-    }
+    // 
 } 
+// public static int Add(int firstNumber, int SecondNumber){
+    //     int sum = firstNumber+SecondNumber; 
+    //     return sum; 
+    // }
